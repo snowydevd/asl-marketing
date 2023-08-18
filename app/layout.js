c19d69dfd80d01604fac23b95cd76557df491e89
Snowie Dev <lautarochini4@gmail.com>
2023-08-17 23:52:26 -0300

    analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({ children }) {
        
         {children}
         <Footer/>
+        <Analytics />
         <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
         </body>
     </html>
