cd8c1bf05b4a8a7c0beea9c75fff23f661aed513
Snowie Dev <lautarochini4@gmail.com>
2023-08-29 12:05:07 -0300

    aslv2

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,5 +1,5 @@
-import connectDB from "@/app/lib/mongodb";
-import Contact from "@/app/models/contact";
+import connectDB from "../../lib/mongodb";
+import Contact from "../../models/contact";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 
